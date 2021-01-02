1a5664b24fea5614208f259812c403c866d31c9e
Mariusz Skuza <72551592+Prestionyk@users.noreply.github.com>
2021-01-02 14:11:29 +0100

    Add files via upload

---
+++ MS/MS/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectGame
+{
+    public class Enemy : EnemyInterface
+    {
+        public string name;
+        protected int HP=100, MAXHP=100, MP=20, MAXMP=20, STR=10, DEF=10, INT=10, AGI=10;
+        
+        public List<int> GetStats()
+        {
+            List<int> stats = new List<int>() {
+                HP, MAXHP, MP, MAXMP, STR, DEF, INT, AGI
+            };
+
+            return stats;
+        }
+
+    }
+}
---
+++ MS/MS/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ProjectGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Skeleton Skeleton = new Skeleton("Skeleton",5,5,5,5,5,5,5,5);
+
+            Fight Round_one = new Fight();
+
+            Round_one += Skeleton;
+
+            Round_one.whosInFight();
+        }
+    }
+}
