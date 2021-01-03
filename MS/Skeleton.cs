41106ad51e2a03910a12176eacd8db3fd3359832
BartoszWiecz <72551967+BartoszWiecz@users.noreply.github.com>
2021-01-03 15:09:48 +0100

    Update Skeleton.cs

--- MS/Skeleton.cs
+++ MS/Skeleton.cs
@@ -6,10 +6,21 @@ namespace ProjectGame
 
         public Skeleton()
         {
+            this.sprite="    ___   " +
+                        "   ,o_ )  " +
+                        "   "' |_  " +
+                        "   _.-/)\ " +
+                        "  "  ,-_/ " +
+                        "   '"   ; " +
+                        "       /  " +
+                        "     /  \ " +
+                        "   .-'  ,/" ;
             this.name = "Skeleton";
             this.HP = 30;
             this.MAXHP = 30;
             this.STR = 8;
+            this.DEF = 6;
+            this.INT = 3;
 
         }
 
