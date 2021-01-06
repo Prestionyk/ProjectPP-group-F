034e603f43c0871dde7b47bc8e27eee3c941e84b
PatrykSzymankowski <72561961+PatrykSzymankowski@users.noreply.github.com>
2021-01-06 19:31:39 +0100

    Add files via upload

---
+++ MS/Dungeon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectGame
+{
+    class Dungeon 
+    {
+        private List<Fight> fightList = new List<Fight>();
+        
+        public Dungeon() { }
+        public static Dungeon operator +=(Dungeon dungeon, Fight fight)
+        {
+            dungeon.fightList.Add(fight);
+            return dungeon;
+        }
+
+       public void Enter()
+        {
+            foreach (Fight fight in fightList)
+                Console.WriteLine(fight);
+        }
+    }
+}
